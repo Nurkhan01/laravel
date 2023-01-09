f2b2f5b195fe06b6c9795fa720276922a96af5b6
Nurkhan01 <zhaksylyk.nurkhan@gmail.com>
2023-01-09 17:46:30 +0600

    first commit

---
+++ app/Http/Controllers/TestController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Test;
+use Illuminate\Http\Request;
+use function GuzzleHttp\Promise\all;
+
+class TestController extends Controller
+{
+    public function index()
+    {
+
+        $flights = Test::where('title', 'Dota2')->get();
+
+        $flights = $flights->reject(function ($flight) {
+            return $flight->cancelled;
+        });
+        dump($flights);
+//        $test = Test::find(1);
+//        $str = 'string';
+//        dump($test->title); // Helper который не остонавливает работу скрипта
+//        echo "hello";
+//        dd($str); // Helper который остонавливает работу скрипта
+    }
+}
