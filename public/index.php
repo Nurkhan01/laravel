fbba5384c075bd33da507862a167a5e7f19a3174
Nurkhan01 <zhaksylyk.nurkhan@gmail.com>
2023-01-25 12:39:14 +0600

    another way to solve cors

--- public/index.php
+++ public/index.php
@@ -5,6 +5,41 @@ use Illuminate\Http\Request;
 
 define('LARAVEL_START', microtime(true));
 
+date_default_timezone_set('Asia/Almaty');
+defined('YII_DEBUG') or define('YII_DEBUG', true);
+defined('YII_ENV') or define('YII_ENV', 'dev');
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+$origin = isset($_SERVER['HTTP_ORIGIN']) ? $_SERVER['HTTP_ORIGIN'] : 'http://'.$_SERVER['SERVER_NAME'];
+$allowed_domains = [
+    'http://localhost:8080',
+    'http://localhost:8081',
+];
+
+if (in_array($origin, $allowed_domains)) {
+    header('Access-Control-Allow-Origin: ' . $origin);
+}
+header("Access-Control-Allow-Methods:  POST, GET, DELETE, PUT, PATCH, OPTIONS, HEAD");
+header("Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With");
+header('Access-Control-Allow-Credentials: true');
+$headers = headers_list();
+// get the content type header
+foreach($headers as $header){
+    if (substr(strtolower($header),0,13) == "content-type:"){
+        list($contentType, $charset) = explode(";", trim(substr($header, 14), 2));
+        if (strtolower(trim($charset)) != "charset=utf-8"){
+            header("Content-Type: ".trim($contentType)."; charset=utf-8");
+        }
+    }
+}
+if ($_SERVER['REQUEST_METHOD'] == "OPTIONS") {
+    header("Access-Control-Allow-Origin: {$_SERVER['HTTP_ORIGIN']}");
+    header("Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With");
+    header("HTTP/1.1 200 OK");
+    die();
+}
 /*
 |--------------------------------------------------------------------------
 | Check If The Application Is Under Maintenance
